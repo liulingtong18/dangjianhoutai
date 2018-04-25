b3bf60a4a030c02597630372e7b1990cb8c7d966
liulingtong18 <815361402@qq.com>
2018-04-25 20:34:32 +0800

    第五次 commit

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/partys');
+mongoose.connect('mongodb://localhost/lingtong');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
